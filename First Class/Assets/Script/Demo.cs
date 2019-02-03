808727adc1acd4674befecf40099b14bc3dda99d
You Jin Song <youjinsong@youjinsongsmbp.wireless.newschool.edu>
2019-02-03 16:38:42 -0500

    works

---
+++ First Class/Assets/Script/Demo.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Demo : MonoBehaviour
+{
+    public Text outPut;
+    public GameObject prefab;
+    public int[] numbers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //int a = 1;
+        //print(a);
+        //a++;
+
+        //string a = "hi";
+        ////string b = " there";
+        //int b = 5;
+        //a += b;
+
+        //float a = 1;
+        //float b = 2;
+        //a = a / b;
+        //int c = (int)Mathf.Ceil(a);
+        //Mathf.Round = round the number
+        //Mathf.Ceil and Mathf.Floor - also for the calculation
+
+        //bool a = true;
+        //a = !(!a != false);
+        //outPut.text = a.ToString();
+        // == means equavelecy/checking
+
+        //int a = 1;
+
+        //if(a == 1){
+        //    outPut.text = "less than 1";
+        //}else if(a<2){
+        //    outPut.text = "just 1";
+        //}else {
+        //    outPut.text = "higher than 1";
+        //}
+
+
+
+        //StartCoroutine(MakeBlocks());
+
+
+        //outPut.text = numbers[4].ToString();
+
+        //int[] numbers = new int[] { 11, 2, 45, 7 };
+
+        //for (int i = numbers.Length - 1; i >- 1; i--){
+        //    outPut.text += numbers[i].ToString() + "\n";
+        //}
+
+        //GameObject[] box = GameObject.FindGameObjectsWithTag("box");
+        //for (int i = 0; i < box.Length; i++){
+        //    Destroy(box[i]);
+        //}
+
+        GameObject[] box = GameObject.FindGameObjectsWithTag("box");
+        foreach(GameObject boxes in box){
+            Destroy(boxes);
+        }
+    }
+
+    //IEnumerator MakeBlocks(){
+    //    for (int i = 0; i < 20; i++)
+    //    {
+    //        for (int j = 0; j < 10; j++)
+    //        {
+    //            Vector3 pos = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+    //            Instantiate(prefab, pos, Quaternion.identity);
+    //        }
+    //        yield return null;
+    //    }
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Week2_Light/Assets/LightSwitch.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LightSwitch : MonoBehaviour
+{
+
+    public Light[] roomlights;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //print("hi");
+            for (int i = 0; i < roomlights.Length; i++){
+                //roomlights[i].enabled = false;
+                StartCoroutine(Flicker(roomlights[i]));
+                //StartCoroutine(Fade(roomlights[i]));
+            }
+        }
+    }
+
+    IEnumerator Fade(Light rl)
+    {
+        while(rl.intensity <0){
+            yield return new WaitForSeconds(.1f);
+            rl.intensity -= 1f;
+            print(rl.intensity);
+        }
+
+    }
+
+    IEnumerator Flicker(Light rl){
+        yield return new WaitForSeconds(.1f);
+        rl.intensity = 0;
+
+        yield return new WaitForSeconds(.1f);
+        rl.intensity = 1;
+
+        yield return new WaitForSeconds(.1f);
+        rl.intensity = 0;
+
+        yield return new WaitForSeconds(.1f);
+        rl.intensity = 1;
+
+        yield return new WaitForSeconds(.1f);
+        rl.intensity = 0;
+
+        yield return new WaitForSeconds(.1f);
+        rl.intensity = 0.5f;
+
+        SceneManager.LoadSceneAsync("CuteLand"); //delete all the back scenes 
+    }
+}
